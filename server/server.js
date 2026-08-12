a67b61fdba323e6f10cd6288a1fd79c01fced423
Aryan <aryan007gauba@gmail.com>
2026-08-12 13:17:28 +0530

    updating cors policy to stop blocking the frontend

--- server/server.js
+++ server/server.js
@@ -29,7 +29,24 @@ const io = new Server(server, {
 });
 
 // Middleware
-app.use(cors({ origin: true, credentials: true }));
+const allowedOrigins = [
+  'https://crm-orpin-eight.vercel.app',
+  'http://localhost:5173',
+  'http://localhost:3000'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json()); 
 
 // Attach `io` to request object so controllers can access it via req.app.get('io')
